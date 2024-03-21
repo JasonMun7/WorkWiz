a2d99cb49ff21975c5c88dc79303435084bc6620
Sahil Hosalli <sh2276@cornell.edu>
2024-03-21 17:07:24 -0400

    Add inv index, clean freelancer.json

--- backend/loader.py
+++ backend/loader.py
@@ -3,6 +3,10 @@ Script to convert dataset CSV to JSON
 '''
 import csv
 import json
+import os
+import pandas as pd
+import sklearn.feature_extraction 
+import numpy
 
 # Open the CSV file
 with open('data/freelancer_job_postings.csv', 'r') as csvfile:
@@ -21,8 +25,25 @@ output = {"jobs": []}
 
 # Open the JSON file for writing
 with open('data/freelancer.json', 'w') as jsonfile:
+    i = 0
     for entry in data:
         json_entry = {fieldname: entry[fieldname] for fieldname in fieldnames}
+        json_entry["projectId"] = i
+        i += 1
         output["jobs"].append(json_entry)
 
     json.dump(output, jsonfile, indent=4)
+
+with open('data/freelancer.json', 'r') as jsonfile:
+    data = json.load(jsonfile)
+    jobs_df = pd.DataFrame(data['jobs'])
+
+#Builds and returns an inverted index 
+def createInvIndex():
+    corpus = []
+    for jd in jobs_df['job_description']:
+        corpus.append(jd)
+    vectorizer = sklearn.feature_extraction.text.TfidfVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    vectorizer.get_feature_names_out()
+    return X
