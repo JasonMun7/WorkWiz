e7653215bd432e470df2d02484b4b6ffa2fa82f5
Andrew Cheung <andrewcheung360@gmail.com>
2024-03-22 23:42:22 -0400

    added cosine search functionality instead of json search

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
+import analysis
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -13,13 +14,12 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'init.json')
+json_file_path = os.path.join(current_directory, 'data/freelancer.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
     data = json.load(file)
-    episodes_df = pd.DataFrame(data['episodes'])
-    reviews_df = pd.DataFrame(data['reviews'])
+    jobs_df = pd.DataFrame(data['jobs'])
 
 app = Flask(__name__)
 CORS(app)
@@ -27,20 +27,33 @@ CORS(app)
 # Sample search using json with pandas
 def json_search(query):
     matches = []
-    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
+    merged_df = jobs_df
+    matches = merged_df[merged_df['job_title'].str.lower().str.contains(query.lower())]
+    matches_filtered = matches[['job_title', 'job_description', 'client_average_rating']]
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
+
+def cosine_search(query):
+    # Use the get_top5 function from the analysis.py file
+    top_matches = analysis.get_top5(query, jobs_df['job_description'].values.tolist(), jobs_df)
+
+    # Create a DataFrame from the top matching jobs
+    top_matches_df = pd.DataFrame(top_matches, columns=['cosine_similarity', 'job_title', 'client_average_rating', 'job_description'])
+
+    # Convert the DataFrame to JSON
+    top_matches_json = top_matches_df.to_json(orient='records')
+    return top_matches_json
+
 
 @app.route("/")
 def home():
     return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
-def episodes_search():
+@app.route("/jobs")
+def jobs_search():
     text = request.args.get("title")
-    return json_search(text)
+    # return json_search(text)
+    return cosine_search(text)
 
 if 'DB_NAME' not in os.environ:
     app.run(debug=True,host="0.0.0.0",port=5000)
